efff9678e679be41c6944ab5ccd8cf80da430def
Lucas <lukspaiva@hotmail.com>
2017-07-10 12:01:09 -0300

    rotas de paciente

--- sae/src/components/Paciente.vue
+++ sae/src/components/Paciente.vue
@@ -10,7 +10,7 @@
           <div id="contenta">
 
               <div class="breadcrumbs">
-                  <a href="/saeinfo">Início</a> › <a href="./pacientes.html">Pacientes</a> › {{paciente.Nome}}
+                  <a href="/saeinfo">Início</a> › <a href="#/saeinfo/pacientes">Pacientes</a> › {{paciente.Nome}}
               </div>
               <!-- Content -->
               <div id="content" class="colM">
@@ -18,7 +18,7 @@
                   <div id="content-main">
                       <ul class="object-tools">
                           <li>
-                              <a href="/SaeApp/paciente/70/history/" class="historylink">Histórico</a>
+                              <a :href="'#/saeinfo/paciente/'+paciente.id+'/historico/'" class="historylink">Histórico</a>
                           </li>
                       </ul>
 
