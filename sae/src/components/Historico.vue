3a1fc0dfce01b8fabf4533cbe7c1d48817366ce1
Lucas <lukspaiva@hotmail.com>
2017-07-18 02:32:34 -0300

    editando historico

--- sae/src/components/Historico.vue
+++ sae/src/components/Historico.vue
@@ -736,6 +736,8 @@ export default {
       // transforma genitalia em string
       this.historico.genitalia = this.historico.genitaliaOpcoes.toString() + ',' + this.historico.genitaliaOutros
 
+      console.log(this.historico)
+
       $.post('http://localhost/sae/editHistorico.php', JSON.stringify(this.historico))
         .then(res => {
           console.log(res)
